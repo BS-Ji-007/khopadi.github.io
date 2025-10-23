d56bd2fd54cb08f06a8258873fea95e51bb8b11a
BS-Ji-007 <bsji007007@gmail.com>
2025-10-23 20:17:18 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import MovieDetails from './pages/MovieDetails';
+
+function App() {
+  return (
+    <Router>
+      <div className="bg-gray-900 text-white min-h-screen">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/movie/:id" element={<MovieDetails />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
